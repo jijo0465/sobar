27462b18f68711f41d9bb7ff04663a26fc601952
jijo0465 <jijo0465@gmail.com>
2019-11-20 01:05:57 +0530

    new user png

--- lib/firebase_services.dart
+++ lib/firebase_services.dart
@@ -1,7 +1,5 @@
-import 'dart:ffi';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:kudians/edited_place.dart';
